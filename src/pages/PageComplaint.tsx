242c6aa24dc709fceb03defce5cb29f1911da0c6
ViniciusFS1 <fioravante.vinicius@gmail.com>
2025-10-19 15:09:34 -0300

    Update PageComplaint.tsx

--- src/pages/PageComplaint.tsx
+++ src/pages/PageComplaint.tsx
@@ -1,43 +1,46 @@
-import { useNavigate, useParams } from "react-router-dom"
-import reclamacoes from "../data/complaintss.json"
-import Complaint from "./Complaint";
-import { useMemo } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import reclamacoes from '../data/complaintss.json';
+import Complaint from './Complaint';
+import { useMemo } from 'react';
 
 type Row = {
-    complaint_title: string;
-    complaint_description: string;
-    complaint_creation_date: string;
-    complaint_solution: string;
-    complaint_num: number;
-    complaint_per: number;
-}
+  complaint_title: string;
+  complaint_description: string;
+  complaint_creation_date: string;
+  complaint_solution: string;
+  complaint_num: number;
+  complaint_per: number;
+};
 
 export default function PageComplaint() {
-    const navegacao = useNavigate();
-    const { id: idParam } = useParams<{id: string}>()
-    const reclamacao = useMemo(() => {
-        if(!idParam) return undefined
-        const lista = reclamacoes as Row[]; 
-        const found = lista.find((r) => {
-            const chave = r.complaint_num
-            return String(chave) === String(idParam)
-        }); 
-        return found; 
-    }, [idParam])
+  const navegacao = useNavigate();
+  const { id: idParam } = useParams<{ id: string }>();
+  const reclamacao = useMemo(() => {
+    if (!idParam) return undefined;
+    const lista = reclamacoes as Row[];
+    const found = lista.find((r) => {
+      const chave = r.complaint_num;
+      return String(chave) === String(idParam);
+    });
+    return found;
+  }, [idParam]);
 
-    if (!reclamacao) {
-        return (
-            <div>
-                <p>reclamacao nao localizada</p>
-                <button onClick={() => navegacao(-1)}>Voltar</button>
-            </div>
-        )
-    }
+  if (!reclamacao) {
     return (
-        <Complaint
-            complaintTitle={reclamacao.complaint_title}
-            complaintText={reclamacao.complaint_description}
-            complaintPercent={reclamacao.complaint_per}
-            numComplaints={reclamacao.complaint_num} />
+      <div>
+        <p>reclamacao nao localizada</p>
+        <button onClick={() => navegacao(-1)}>Voltar</button>
+      </div>
     );
+  }
+  return (
+    <Complaint
+      complaintTitle={reclamacao.complaint_title}
+      complaintText={reclamacao.complaint_description}
+      complaintPercent={reclamacao.complaint_per}
+      numComplaints={reclamacao.complaint_num}
+      complaintsolution={reclamacao.complaint_solution}
+    />
+  );
 }
+
