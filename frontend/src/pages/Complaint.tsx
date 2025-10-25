de3b30292c1961b61b590aec191c717571e74a5b
ViniciusFS1 <fioravante.vinicius@gmail.com>
2025-10-25 17:47:35 -0300

    Update Complaint.tsx

--- frontend/src/pages/Complaint.tsx
+++ frontend/src/pages/Complaint.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/advice_page.css';
+import { GoogleGenAI } from '@google/genai';
+
 function BoldTextWithLineBreaks({ text }: { text: string }) {
   // Primeiro, separamos o texto por ** para identificar bold
   const boldParts = text.split(/\*\*(.*?)\*\*/g);
@@ -8,7 +10,7 @@ function BoldTextWithLineBreaks({ text }: { text: string }) {
     <>
       {boldParts.map((part: string, i: number) => {
         // Agora, para cada parte, dividimos por \n
-        const lines = part.split("\n");
+        const lines = part.split('\n');
 
         return lines.map((line: string, j: number) => {
           // Se for índice ímpar, é bold
@@ -35,6 +37,18 @@ type ComplaintProps = {
   complaintsolution: string;
 };
 
+async function ha() {
+  const GEMINI_API_KEY = ' ';
+  const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash-001',
+    contents: 'Why is the sky blue?',
+  });
+  console.log(response.text);
+  console.log('Why is the sky blue?');
+alert('Why is the sky blue?');
+}
+
 export default function Complaint({
   complaintTitle,
   complaintText,
@@ -45,7 +59,9 @@ export default function Complaint({
   return (
     <div className="meubody">
       <header>
-        <button className="back-button" onClick={() => window.history.back()}>Voltar</button>
+        <button className="back-button" onClick={() => window.history.back()}>
+          Voltar
+        </button>
         <h1>Recomendações</h1>
       </header>
 
@@ -62,6 +78,11 @@ export default function Complaint({
         <p>
           <BoldTextWithLineBreaks text={complaintsolution} />
         </p>
+        <form>
+          <div>
+            <button onClick={ha()}> 'Salvando...' </button>
+          </div>
+        </form>
       </main>
     </div>
   );
