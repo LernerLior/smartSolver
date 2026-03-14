6ba491ffc63b31729243fcfdc17ed501542ad06e
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-03-14 14:23:54 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import PageComplaint from './pages/PageComplaint'
+import PageGraphics from './pages/PageGraphics'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard/>} />
       <Route path="/complaint/:id" element={<PageComplaint/>} />
+      <Route path="/graphics" element={<PageGraphics/>} />
     </Routes>
   )
 }
