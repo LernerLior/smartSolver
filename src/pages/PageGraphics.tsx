3d7bc788594f840ab9015bc127d9fab81a62e134
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-03-14 16:59:14 -0300

    Update PageGraphics.tsx

--- src/pages/PageGraphics.tsx
+++ src/pages/PageGraphics.tsx
@@ -1,18 +1,27 @@
+import '../styles/graphics.css';
 import { useNavigate, useParams } from 'react-router-dom';
-import TwoLevelPieChart from './Graphics';
+import LineC from './Graphics';
 import { useMemo } from 'react';
 
 
 export default function PageGraphics() {
 
   return (
-      <div>	
-      <header>
+<body className="layout2">
+	
+      <header className="head">
       	<h2>PageGraphics</h2>
       </header>
-      <div>
-      	<TwoLevelPieChart />
-      </div>
-      </div>
+
+
+	<main className="main2">
+		<section className="graph2">	
+      			<LineC/>
+      		</section>
+      	</main>
+
+
+
+</body>
   );
 }
