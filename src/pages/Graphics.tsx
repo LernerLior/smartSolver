d140164d105aeb589f98f3e554f422f0c6883d28
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-03-14 16:58:50 -0300

    Update Graphics.tsx

--- src/pages/Graphics.tsx
+++ src/pages/Graphics.tsx
@@ -1,64 +1,74 @@
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { Pie, PieChart, Tooltip} from 'recharts';
+import { LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line } from 'recharts';
 import { RechartsDevtools } from '@recharts/devtools';
 
 // #region Sample data
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
-const data02 = [
-  { name: 'A1', value: 100 },
-  { name: 'A2', value: 300 },
-  { name: 'B1', value: 100 },
-  { name: 'B2', value: 80 },
-  { name: 'B3', value: 40 },
-  { name: 'B4', value: 30 },
-  { name: 'B5', value: 50 },
-  { name: 'C1', value: 100 },
-  { name: 'C2', value: 200 },
-  { name: 'D1', value: 150 },
-  { name: 'D2', value: 50 },
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
 ];
 
-// #endregion
-export default function TwoLevelPieChart({
-  isAnimationActive,
-  defaultIndex,
-}: {
-  isAnimationActive?: boolean;
-  defaultIndex?: TooltipIndex;
-}) {
+export default function LineC(isAnimationActive = true){
+
   return (
-    <PieChart
-      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
-      responsive
-    >
-      <Pie
-        data={data01}
-        dataKey="value"
-        cx="50%"
-        cy="50%"
-        outerRadius="50%"
-        fill="#8884d8"
-        isAnimationActive={isAnimationActive}
-      />
-      <Pie
-        data={data02}
-        dataKey="value"
-        cx="50%"
-        cy="50%"
-        innerRadius="60%"
-        outerRadius="80%"
-        fill="#82ca9d"
-        label
-        isAnimationActive={isAnimationActive}
-      />
-      <Tooltip defaultIndex={defaultIndex} />
-      <RechartsDevtools />
-    </PieChart>
-  );
+  <LineChart
+    style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+    responsive
+    data={data}
+    margin={{
+      top: 5,
+      right: 30,
+      left: 20,
+      bottom: 5,
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis width="auto" />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="pv" stroke="#8884d8" isAnimationActive={isAnimationActive} />
+    <Line type="monotone" dataKey="uv" stroke="#82ca9d" isAnimationActive={isAnimationActive} />
+    <RechartsDevtools />
+  </LineChart>
+);
 }
