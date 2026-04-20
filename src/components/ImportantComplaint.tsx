8f0d22412ce18779c2c629a68b62418ef545be19
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-20 17:45:59 -0300

    Update ImportantComplaint.tsx

--- src/components/ImportantComplaint.tsx
+++ src/components/ImportantComplaint.tsx
@@ -1,32 +1,40 @@
+import '../styles/dashboard.css';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-type complaint2Props = {
+interface ComplaintProps2{
+  id: string;
+  complaint_title: string;
+  complaint_description: string;
+  complaint_creation_date: string;
+  complaint_solution: string;
+  complaint_category: string;
+  complaint_importance: number;
+};
 
-    "complaint_title": string;
-    "complaint_description": string;
-    "complaint_creation_date": string;
-    "complaint_solution": string;
-    "complaint_num": string;
-    "complaint_per": string;
-
-
-}
-
-type complaintProps = {
-
-    "complaint_title": string;
-
-
-}
+interface ComplaintProps{
+  complaint_title: string,
+  complaint_description: string,
+  complaint_importance: number
+};
 
 
-
-export default function ImportantComplaint(props: complaintProps){
-
+export default function ImportantComplaint(props : ComplaintProps){
+	
+	
 
 	return (
 
 		<div>
-			<p>{props.complaint_title}</p>	
+		
+			{props.complaint_importance == 5 ? 
+			(<div><h4>{props.complaint_title}</h4>
+			<p className="teste">{props.complaint_description}</p>
+			 <button className="read-more-btn" onClick={() => carregarbotao(reclamacao.id)}>
+                    		Ler mais e obter recomendações
+                  	</button></div>)
+			: (<div></div>)
+			}
 		</div>
 		
  	 );
