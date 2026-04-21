b0080981e0e84c87e53e8ef53ba35b1a02a0f151
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-21 14:58:17 -0300

    Update ImportantComplaint.tsx

--- src/components/ImportantComplaint.tsx
+++ src/components/ImportantComplaint.tsx
@@ -2,16 +2,6 @@ import '../styles/dashboard.css';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-interface ComplaintProps2{
-  id: string;
-  complaint_title: string;
-  complaint_description: string;
-  complaint_creation_date: string;
-  complaint_solution: string;
-  complaint_category: string;
-  complaint_importance: number;
-};
-
 interface ComplaintProps{
   complaint_title: string,
   complaint_description: string,
@@ -28,7 +18,7 @@ export default function ImportantComplaint(props : ComplaintProps){
 		<div className="complaint-card">
 		
 			{props.complaint_importance == 5 ? 
-			(<div><h4>{props.complaint_title}</h4>
+			(<div className="correct-size"><h4>{props.complaint_title}</h4>
 			<p className="teste">{props.complaint_description}</p>
 			 <button className="read-more-btn" onClick={() => carregarbotao(reclamacao.id)}>
                     		Ler mais e obter recomendações
